017f43ea51f98c9a24bb3c7af194af401ebe0612
Tomy Rhymond <trhymond@gmail.com>
2017-07-07 12:38:05 -0400

    Adding Samples Project

---
+++ PowershellWrapper.Samples/ActiveDirectorySample.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Powershell;
+using Powershell.Connectors;
+using Powershell.Model;
+
+namespace PowershellWrapper.Samples
+{
+    public class ActiveDirectorySample
+    {
+
+        public static void Run()
+        {
+            var userName = "<AD User Name>";
+            var password = "<AD Password>";
+
+            var connector = new ActiveDirectoryConnector(userName, password);
+
+            using (var client = PowerShellClient.Create(connector))
+            {
+                var geUserCommand = new PowershellCommand
+                {
+                    Name = "GetUser",
+                    CommandText = "Get-User",
+                    CommandParameters = new List<PowershellCommandParameter> {
+                                    new PowershellCommandParameter { Name = "ResultSize", Value = "unlimited" }, //"100"
+                                    new PowershellCommandParameter { Name = "OrganizationalUnit", Value = "Marketing" } 
+                            }
+                };
+
+                var result = client.Execute<ActiveDirectory>(new List<PowershellCommand> { geUserCommand });
+
+                if(result.Status == StatusCode.SUCCESS)
+                {
+                    var adUsers = (List<ActiveDirectory>)result.Content;
+                }
+            }
+        }
+    }
+
+    public class ActiveDirectory
+    {
+        public int Id { get; set; }
+        public string SamAccountName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DisplayName { get; set; }
+        public string DistinguishedName { get; set; }
+        public string UserPrincipalName { get; set; }
+        public string Title { get; set; }
+        public string ManagerId { get; set; }
+        public string ManagerName { get; set; }
+        public string ManagerEmailAddress { get; set; }
+        public string CompanyName { get; set; }
+        public string Organization { get; set; }
+        public string Department { get; set; }
+        public string Division { get; set; }
+        public string PhysicalDeliveryOfficeName { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string CountryOrRegion { get; set; }
+        public string TelephoneNumber { get; set; }
+        public string MobilePhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public Nullable<System.DateTime> LastLogon { get; set; }
+        public Nullable<System.DateTime> AccountExpires { get; set; }
+        public string Status { get; set; }
+        public string HomeDirectory { get; set; }
+        public string HomeDrive { get; set; }
+        public string Description { get; set; }
+        public string EmployeeID { get; set; }
+    }
+}
